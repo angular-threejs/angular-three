54901e78c03366805746e7fb36ffd94742bf1077
Chau Tran <nartc7789@gmail.com>
2023-01-20 16:00:39 -0600

    docs: first scene

---
+++ apps/documentation/src/components/CodeDemo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function CodeDemo({ src, srcId, title }) {
+    const iframeSrc = srcId ? `https://stackblitz.com/edit/angular-three-demo-template-${srcId}` : src;
+
+    return (
+        <iframe
+            className="code-demo"
+            src={iframeSrc.concat(
+                '?ctl=1&embed=1&file=src/app/scene.component.ts&hideExplorer=1&hideNavigation=1&view=preview'
+            )}
+            title={'angular_three_'.concat(title || Math.random().toString())}
+        ></iframe>
+    );
+}
