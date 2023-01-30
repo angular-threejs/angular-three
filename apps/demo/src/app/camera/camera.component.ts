4553199cb75a8968d1a883a52203b068b4d26d94
Chau Tran <nartc7789@gmail.com>
2023-01-29 23:47:38 -0600

    docs: clean up camera demo

--- apps/demo/src/app/camera/camera.component.ts
+++ apps/demo/src/app/camera/camera.component.ts
@@ -5,9 +5,10 @@ import {
     ElementRef,
     HostListener,
     inject,
+    OnInit,
     ViewChild,
 } from '@angular/core';
-import { injectBeforeRender, injectNgtRef, NgtArgs, NgtCanvas, NgtState, NgtStore } from 'angular-three';
+import { injectBeforeRender, NgtArgs, NgtCanvas, NgtRenderState, NgtState, NgtStore } from 'angular-three';
 import * as THREE from 'three';
 
 @Component({
@@ -15,16 +16,16 @@ import * as THREE from 'three';
     template: `
         <ngt-group #cameras>
             <ngt-perspective-camera
-                [ref]="cameraPerspectiveRef"
+                #perspectiveCamera
                 [aspect]="store.get('viewport', 'aspect') * 0.5"
                 [near]="150"
                 [far]="1000"
                 [rotation]="[0, Math.PI, 0]"
             />
             <ngt-orthographic-camera
-                [ref]="cameraOrthographicRef"
-                [left]="(0.5 * 600 * store.get('viewport', 'aspect')) / -2"
-                [right]="(0.5 * 600 * store.get('viewport', 'aspect')) / 2"
+                #orthographicCamera
+                [left]="(300 * store.get('viewport', 'aspect')) / -2"
+                [right]="(300 * store.get('viewport', 'aspect')) / 2"
                 [top]="300"
                 [bottom]="-300"
                 [near]="150"
@@ -38,10 +39,10 @@ import * as THREE from 'three';
             </ngt-mesh>
         </ngt-group>
 
-        <ngt-camera-helper #perspective *args="[cameraPerspectiveRef.nativeElement]" />
-        <ngt-camera-helper #orthographic *args="[cameraOrthographicRef.nativeElement]" />
+        <ngt-camera-helper #perspectiveHelper *args="[perspectiveCamera]" />
+        <ngt-camera-helper #orthographicHelper *args="[orthographicCamera]" />
 
-        <ngt-mesh #whiteMesh>
+        <ngt-mesh #mesh>
             <ngt-sphere-geometry *args="[100, 16, 8]" />
             <ngt-mesh-basic-material [wireframe]="true" />
 
@@ -59,19 +60,18 @@ import * as THREE from 'three';
     imports: [NgtArgs],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class Scene implements AfterViewInit {
+export class Scene implements AfterViewInit, OnInit {
     readonly Math = Math;
-
-    readonly cameraPerspectiveRef = injectNgtRef<THREE.PerspectiveCamera>();
-    readonly cameraOrthographicRef = injectNgtRef<THREE.OrthographicCamera>();
 
     readonly store = inject(NgtStore);
 
+    @ViewChild('perspectiveCamera', { static: true }) perspectiveCamera!: ElementRef<THREE.PerspectiveCamera>;
+    @ViewChild('orthographicCamera', { static: true }) orthographicCamera!: ElementRef<THREE.OrthographicCamera>;
     @ViewChild('cameras', { static: true }) cameraGroup!: ElementRef<THREE.Group>;
-    @ViewChild('perspective') cameraPerspectiveHelper?: ElementRef<THREE.CameraHelper>;
-    @ViewChild('orthographic') cameraOrthographicHelper?: ElementRef<THREE.CameraHelper>;
+    @ViewChild('mesh', { static: true }) mesh!: ElementRef<THREE.Mesh>;
 
-    @ViewChild('whiteMesh', { static: true }) whiteMesh!: ElementRef<THREE.Mesh>;
+    @ViewChild('perspectiveHelper') perspectiveHelper?: ElementRef<THREE.CameraHelper>;
+    @ViewChild('orthographicHelper') orthographicHelper?: ElementRef<THREE.CameraHelper>;
 
     readonly vertices: number[] = [];
 
@@ -79,107 +79,109 @@ export class Scene implements AfterViewInit {
     private activeCameraHelper?: THREE.CameraHelper;
 
     constructor() {
+        injectBeforeRender(this.onBeforeRender.bind(this), 1);
+    }
+
+    ngOnInit() {
         for (let i = 0; i < 10000; i++) {
             this.vertices.push(THREE.MathUtils.randFloatSpread(2000)); // x
             this.vertices.push(THREE.MathUtils.randFloatSpread(2000)); // y
             this.vertices.push(THREE.MathUtils.randFloatSpread(2000)); // z
         }
-        injectBeforeRender(({ gl, size, scene, camera }) => {
-            const r = Date.now() * 0.0005;
-            const mesh = this.whiteMesh.nativeElement;
-            const cameraGroup = this.cameraGroup.nativeElement;
-            const cameraPerspective = this.cameraPerspectiveRef.nativeElement;
-            const cameraPerspectiveHelper = this.cameraPerspectiveHelper?.nativeElement;
-            const cameraOrthographic = this.cameraOrthographicRef.nativeElement;
-            const cameraOrthographicHelper = this.cameraOrthographicHelper?.nativeElement;
-
-            mesh.position.x = 700 * Math.cos(r);
-            mesh.position.z = 700 * Math.sin(r);
-            mesh.position.y = 700 * Math.sin(r);
-
-            mesh.children[0].position.x = 70 * Math.cos(2 * r);
-            mesh.children[0].position.z = 70 * Math.sin(r);
-
-            if (cameraPerspective && cameraOrthographic && cameraPerspectiveHelper && cameraOrthographicHelper) {
-                if (this.activeCamera === cameraPerspective) {
-                    cameraPerspective.fov = 35 + 30 * Math.sin(0.5 * r);
-                    cameraPerspective.far = mesh.position.length();
-                    cameraPerspective.updateProjectionMatrix();
-
-                    cameraPerspectiveHelper.update();
-                    cameraPerspectiveHelper.visible = true;
-
-                    cameraOrthographicHelper.visible = false;
-                } else {
-                    cameraOrthographic.far = mesh.position.length();
-                    cameraOrthographic.updateProjectionMatrix();
-
-                    cameraOrthographicHelper.update();
-                    cameraOrthographicHelper.visible = true;
-
-                    cameraPerspectiveHelper.visible = false;
-                }
-            }
-
-            cameraGroup.lookAt(mesh.position);
-
-            gl.clear();
-
-            this.activeCameraHelper!.visible = false;
-            gl.setViewport(0, 0, size.width / 2, size.height);
-            gl.render(scene, this.activeCamera!);
-
-            this.activeCameraHelper!.visible = true;
-
-            gl.setViewport(size.width / 2, 0, size.width / 2, size.height);
-            gl.render(scene, camera);
-        }, 1);
     }
 
     ngAfterViewInit() {
-        this.activeCamera = this.cameraPerspectiveRef.nativeElement;
-        this.activeCameraHelper = this.cameraPerspectiveHelper?.nativeElement;
+        this.activeCamera = this.perspectiveCamera.nativeElement;
+        this.activeCameraHelper = this.perspectiveHelper?.nativeElement;
     }
 
     @HostListener('document:keydown', ['$event'])
-    onKeyDown(event: KeyboardEvent) {
-        if (event.key.toLowerCase() === 'o') {
-            this.activeCamera = this.cameraOrthographicRef.nativeElement;
-            this.activeCameraHelper = this.cameraOrthographicHelper?.nativeElement;
-        } else if (event.key.toLowerCase() === 'p') {
-            this.activeCamera = this.cameraPerspectiveRef.nativeElement;
-            this.activeCameraHelper = this.cameraPerspectiveHelper?.nativeElement;
+    onKeyDown({ key }: KeyboardEvent) {
+        switch (key.toLowerCase()) {
+            case 'o':
+                this.activeCamera = this.orthographicCamera.nativeElement;
+                this.activeCameraHelper = this.orthographicHelper?.nativeElement;
+                break;
+            case 'p':
+                this.activeCamera = this.perspectiveCamera.nativeElement;
+                this.activeCameraHelper = this.perspectiveHelper?.nativeElement;
         }
+    }
+
+    private onBeforeRender({ gl, size, camera, scene }: NgtRenderState) {
+        if (!this.activeCamera || !this.activeCameraHelper) return;
+
+        const r = Date.now() * 0.0005;
+        // reassign shorthands
+        const mesh = this.mesh.nativeElement;
+        const camGroup = this.cameraGroup.nativeElement;
+        const perspective = this.perspectiveCamera.nativeElement;
+        const perspectiveHelper = this.perspectiveHelper?.nativeElement;
+        const orthographic = this.orthographicCamera.nativeElement;
+        const orthographicHelper = this.orthographicHelper?.nativeElement;
+
+        mesh.position.x = 700 * Math.cos(r);
+        mesh.position.z = 700 * Math.sin(r);
+        mesh.position.y = 700 * Math.sin(r);
+
+        mesh.children[0].position.x = 70 * Math.cos(2 * r);
+        mesh.children[0].position.z = 70 * Math.sin(r);
+
+        if (perspective && orthographic && perspectiveHelper && orthographicHelper) {
+            if (this.activeCamera === perspective) {
+                perspective.fov = 35 + 30 * Math.sin(0.5 * r);
+                perspective.far = mesh.position.length();
+                perspective.updateProjectionMatrix();
+
+                perspectiveHelper.update();
+                perspectiveHelper.visible = true;
+
+                orthographicHelper.visible = false;
+            } else {
+                orthographic.far = mesh.position.length();
+                orthographic.updateProjectionMatrix();
+
+                orthographicHelper.update();
+                orthographicHelper.visible = true;
+
+                perspectiveHelper.visible = false;
+            }
+        }
+
+        camGroup.lookAt(mesh.position);
+
+        gl.clear();
+
+        this.activeCameraHelper.visible = false;
+        gl.setViewport(0, 0, size.width / 2, size.height);
+        gl.render(scene, this.activeCamera);
+
+        this.activeCameraHelper.visible = true;
+
+        gl.setViewport(size.width / 2, 0, size.width / 2, size.height);
+        gl.render(scene, camera);
     }
 }
 
 @Component({
     standalone: true,
     template: `
+        <div class="absolute text-center text-white w-full top-0 left-0 text-xl">
+            <a href="https://threejs.org" target="_blank" rel="noopener" class="underline"> three.js </a> -
+            <a href="https://angular-threejs.netlify.app" target="_blank" rel="noopener" class="underline">
+                Angular Three
+            </a>
+            - cameras
+            <br />
+            <b class="text-green-300">O</b> orthographic <b class="text-green-300">P</b> perspective
+        </div>
         <div class="block h-full w-full bg-black">
             <ngt-canvas
                 [sceneGraph]="SceneGraph"
                 [gl]="{ alpha: false }"
-                [camera]="{
-                    fov: 50,
-                    near: 1,
-                    far: 10000,
-                    position: [0, 0, 2500]
-                }"
+                [camera]="{ fov: 50, near: 1, far: 10000, position: [0, 0, 2500] }"
                 (created)="onCreated($event)"
             />
-        </div>
-        <div class="absolute text-center text-white w-full top-0 left-0 text-xl">
-            <a href="https://threejs.org" target="_blank" rel="noopener" class="underline">three.js</a> -<a
-                href="https://angular-threejs.netlify.app"
-                target="_blank"
-                rel="noopener"
-                class="underline"
-                >Angular Three</a
-            >
-            - cameras
-            <br />
-            <b class="text-green-300">O</b> orthographic <b class="text-green-300">P</b> perspective
         </div>
     `,
     imports: [NgtCanvas],
