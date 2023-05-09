d5e3f18ac7d5451498d7f8492d87e18cd5c38860
Chau Tran <chautran@Chaus-MacBook-Pro.local>
2023-05-09 11:40:31 -0500

    chore(soba): init storybook

---
+++ libs/soba/.storybook/main.js
@@ -0,0 +1,14 @@
+const config = {
+    stories: ['../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+    addons: ['@storybook/addon-essentials'],
+    framework: {
+        name: '@storybook/angular',
+        options: {},
+    },
+};
+
+export default config;
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/packages/storybook/documents/custom-builder-configs
