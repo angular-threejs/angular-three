b12da78241a9a783be7a8c6bc7d7f5cc7cc9ee43
Chau Tran <chautran@Chaus-MacBook-Pro.local>
2023-05-09 11:53:44 -0500

    chore(soba): configure storybook

--- libs/soba/.storybook/main.js
+++ libs/soba/.storybook/main.js
@@ -1,6 +1,19 @@
 const config = {
     stories: ['../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
     addons: ['@storybook/addon-essentials'],
+    webpackFinal: async (config) => {
+        // apply any global webpack configs that might have been specified in .storybook/main.js
+
+        // add your own webpack tweaks if needed
+        config.module.rules.push({
+            test: /\.(glsl|vs|fs|vert|frag)$/,
+            exclude: /node_modules/,
+            use: ['raw-loader', 'glslify-loader'],
+            include: resolve(__dirname, '../'),
+        });
+        return config;
+    },
+    staticDirs: ['./public'],
     framework: {
         name: '@storybook/angular',
         options: {},
