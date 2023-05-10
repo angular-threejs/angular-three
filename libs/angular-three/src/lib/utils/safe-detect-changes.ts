a9214c50389ececfc9983c515ff6c26b1f9e11ec
Chau Tran <chautran@Chaus-MacBook-Pro.local>
2023-05-10 09:36:04 -0500

    fix(core): adjust safe detect changes

--- libs/angular-three/src/lib/utils/safe-detect-changes.ts
+++ libs/angular-three/src/lib/utils/safe-detect-changes.ts
@@ -4,9 +4,7 @@ import type { NgtAnyRecord } from '../types';
 export function safeDetectChanges(cdr: ChangeDetectorRef | undefined | null) {
     if (!cdr) return;
     try {
-        // dynamic created component with ViewContainerRef#createComponent does not have Context
-        // but it has _attachedToViewContainer
-        if ((cdr as NgtAnyRecord)['context'] || (cdr as NgtAnyRecord)['_attachedToViewContainer']) {
+        if ((cdr as NgtAnyRecord)['_attachedToViewContainer']) {
             cdr.detectChanges();
         }
     } catch (e) {
