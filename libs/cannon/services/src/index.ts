9805f50720bb8a9642632ea155d556db3c748b63
Chau Tran <chautran@Chaus-MacBook-Pro.local>
2023-05-19 15:41:23 -0500

    feat(cannon): migrate ray

--- libs/cannon/services/src/index.ts
+++ libs/cannon/services/src/index.ts
@@ -1,3 +1,4 @@
 export * from './body';
 export * from './constraint';
+export * from './ray';
 export * from './spring';
---
+++ libs/cannon/services/src/ray.ts
@@ -0,0 +1,42 @@
+import { Injector, effect, inject, runInInjectionContext, untracked } from '@angular/core';
+import { RayMode, RayOptions, RayhitEvent } from '@pmndrs/cannon-worker-api';
+import { NgtAnyRecord, assertInjectionContext, makeId } from 'angular-three';
+import { NGTC_PHYSICS_API } from 'angular-three-cannon';
+
+export interface NgtcRayOptions {
+    options: () => RayOptions;
+    callback: (e: RayhitEvent) => void;
+    injector?: Injector;
+    deps?: () => NgtAnyRecord;
+}
+
+export function injectRaycastClosest(opts: NgtcRayOptions) {
+    return injectRay('Closest', opts);
+}
+
+export function injectRaycastAny(opts: NgtcRayOptions) {
+    return injectRay('Any', opts);
+}
+
+export function useRaycastAll(opts: NgtcRayOptions) {
+    return injectRay('All', opts);
+}
+
+function injectRay(mode: RayMode, { options, callback, deps = () => ({}), injector }: NgtcRayOptions) {
+    injector = assertInjectionContext(injectRay, injector);
+    return runInInjectionContext(injector, () => {
+        const physicsApi = inject(NGTC_PHYSICS_API);
+        const { worker, events } = physicsApi();
+        const uuid = makeId();
+
+        effect((onCleanup) => {
+            deps();
+            events[uuid] = { rayhit: callback };
+            worker.addRay({ props: { ...untracked(options), mode }, uuid });
+            onCleanup(() => {
+                worker.removeRay({ uuid });
+                delete events[uuid];
+            });
+        });
+    });
+}
