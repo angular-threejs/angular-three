79afb99cd4547fc171584bde42d8ac05d17bf369
Chau Tran <nartc7789@gmail.com>
2023-12-04 22:14:58 -0600

    init docs

---
+++ apps/docs/astro.config.mjs
@@ -0,0 +1,30 @@
+import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			expressiveCode: {
+				themes: ['vitesse-dark', 'vitesse-light'],
+			},
+		}),
+	],
+});
