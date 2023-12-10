fe3c21315fa1d24c9fd64a4ae3a953b636fa5751
Chau Tran <nartc7789@gmail.com>
2023-12-09 19:37:08 -0600

    update packages

--- apps/examples/vite.config.ts
+++ apps/examples/vite.config.ts
@@ -5,21 +5,15 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
 	return {
 		publicDir: 'src/public',
-
 		build: {
+			outDir: '../../dist/apps/examples/client',
+			reportCompressedSize: true,
+			commonjsOptions: { transformMixedEsModules: true },
 			target: ['es2020'],
 		},
-		plugins: [
-			analog({
-				ssr: false,
-			}),
-			tsConfigPaths({
-				root: '../../',
-			}),
-			splitVendorChunkPlugin(),
-		],
+		plugins: [analog({ ssr: false }), tsConfigPaths({ root: '../../' }), splitVendorChunkPlugin()],
 	};
 });
