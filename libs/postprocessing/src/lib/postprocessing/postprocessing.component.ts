5bfda2f05d04cb7e6caea43a797d0d45caa06c5f
ctran <nartc@ctrans-MacBook-Pro.local>
2024-06-20 14:27:28 -0500

    gen lib

---
+++ libs/postprocessing/src/lib/postprocessing/postprocessing.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'lib-postprocessing',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './postprocessing.component.html',
+	styleUrl: './postprocessing.component.css',
+})
+export class PostprocessingComponent {}
