ff4c82a281aa68398b9c8a11014a87f5f8db06f9
ctran <nartc@ctrans-MacBook-Pro.local>
2024-07-08 20:31:09 -0500

    ionic works

---
+++ apps/ionic-app/src/app/home/experience.ts
@@ -0,0 +1,158 @@
+import {
+	CUSTOM_ELEMENTS_SCHEMA,
+	ChangeDetectionStrategy,
+	Component,
+	ElementRef,
+	computed,
+	input,
+	viewChild,
+} from '@angular/core';
+import { Triplet } from '@pmndrs/cannon-worker-api';
+import { NgtArgs, extend, injectBeforeRender } from 'angular-three';
+import { NgtcPhysics } from 'angular-three-cannon';
+import { injectBox, injectPlane, injectSphere } from 'angular-three-cannon/body';
+import { NgtcDebug } from 'angular-three-cannon/debug';
+import * as THREE from 'three';
+import { Color, InstancedMesh, Mesh } from 'three';
+
+const niceColors = ['#99b898', '#fecea8', '#ff847c', '#e84a5f', '#2a363b'] as const;
+
+extend(THREE);
+
+@Component({
+	selector: 'app-plane',
+	standalone: true,
+	template: `
+		<ngt-mesh #mesh [receiveShadow]="true">
+			<ngt-plane-geometry *args="[1000, 1000]" />
+			<ngt-mesh-phong-material [color]="color()" />
+		</ngt-mesh>
+	`,
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [NgtArgs],
+})
+export class Plane {
+	color = input.required<string>();
+	position = input<Triplet>([0, 0, 0]);
+	rotation = input<Triplet>([0, 0, 0]);
+
+	mesh = viewChild.required<ElementRef<Mesh>>('mesh');
+	plane = injectPlane(
+		() => ({
+			position: this.position(),
+			rotation: this.rotation(),
+		}),
+		this.mesh,
+	);
+}
+
+@Component({
+	selector: 'app-box',
+	standalone: true,
+	template: `
+		<ngt-mesh #mesh [castShadow]="true" [receiveShadow]="true">
+			<ngt-box-geometry *args="args" />
+			<ngt-mesh-lambert-material color="white" />
+		</ngt-mesh>
+	`,
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [NgtArgs],
+})
+export class Box {
+	args: Triplet = [4, 4, 4];
+	mesh = viewChild.required<ElementRef<Mesh>>('mesh');
+	boxApi = injectBox(() => ({ args: this.args, mass: 1, type: 'Kinematic' }), this.mesh);
+
+	constructor() {
+		injectBeforeRender(({ clock }) => {
+			const api = this.boxApi();
+			if (!api) return;
+			const t = clock.getElapsedTime();
+			api.position.set(Math.sin(t * 2) * 5, Math.cos(t * 2) * 5, 3);
+			api.rotation.set(Math.sin(t * 6), Math.cos(t * 6), 0);
+		});
+	}
+}
+
+@Component({
+	selector: 'app-instanced-spheres',
+	standalone: true,
+	template: `
+		<ngt-instanced-mesh
+			#instancedMesh
+			[castShadow]="true"
+			[receiveShadow]="true"
+			*args="[undefined, undefined, count()]"
+		>
+			<ngt-sphere-geometry *args="[1, 16, 16]">
+				<ngt-instanced-buffer-attribute attach="attributes.color" *args="[colors(), 3]" />
+			</ngt-sphere-geometry>
+			<ngt-mesh-phong-material [vertexColors]="true" />
+		</ngt-instanced-mesh>
+	`,
+	imports: [NgtArgs],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InstancedSpheres {
+	count = input(100);
+
+	instancedMesh = viewChild<ElementRef<InstancedMesh>>('instancedMesh');
+	sphere = injectSphere(
+		(index) => ({
+			args: [1],
+			mass: 1,
+			position: [Math.random() - 0.5, Math.random() - 0.5, index * 2],
+		}),
+		this.instancedMesh,
+	);
+
+	colors = computed(() => {
+		const array = new Float32Array(this.count() * 3);
+		const color = new Color();
+		for (let i = 0; i < this.count(); i++) {
+			color
+				.convertSRGBToLinear()
+				.set(niceColors[Math.floor(Math.random() * 5)])
+				.toArray(array, i * 3);
+		}
+		return array;
+	});
+}
+
+@Component({
+	standalone: true,
+	template: `
+		<ngt-hemisphere-light [intensity]="0.35 * Math.PI" />
+		<ngt-spot-light
+			[angle]="0.3"
+			[castShadow]="true"
+			[decay]="0"
+			[intensity]="2 * Math.PI"
+			[penumbra]="1"
+			[position]="[30, 0, 30]"
+		>
+			<ngt-vector2 *args="[256, 256]" attach="shadow.mapSize" />
+		</ngt-spot-light>
+		<ngt-point-light [decay]="0" [intensity]="0.5 * Math.PI" [position]="[-30, 0, -30]" />
+		<ngtc-physics [options]="{ gravity: [0, 0, -30] }">
+			<app-plane [color]="niceColors[4]" />
+			<app-plane [color]="niceColors[1]" [position]="[-6, 0, 0]" [rotation]="[0, 0.9, 0]" />
+			<app-plane [color]="niceColors[2]" [position]="[6, 0, 0]" [rotation]="[0, -0.9, 0]" />
+			<app-plane [color]="niceColors[3]" [position]="[0, 6, 0]" [rotation]="[0.9, 0, 0]" />
+			<app-plane [color]="niceColors[0]" [position]="[0, -6, 0]" [rotation]="[-0.9, 0, 0]" />
+			<app-box />
+			<app-instanced-spheres />
+		</ngtc-physics>
+	`,
+	imports: [InstancedSpheres, Box, Plane, NgtcPhysics, NgtArgs, NgtcDebug],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	host: { class: 'kimenatic-experience' },
+})
+export class Experience {
+	Math = Math;
+	niceColors = niceColors;
+}
--- apps/ionic-app/src/app/home/home.page.ts
+++ apps/ionic-app/src/app/home/home.page.ts
@@ -1,32 +1,10 @@
-import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, viewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { extend, injectBeforeRender, NgtCanvas } from 'angular-three';
+import { extend, NgtCanvas } from 'angular-three';
 import * as THREE from 'three';
-import { Mesh } from 'three';
+import { Experience } from './experience';
 
 extend(THREE);
-
-@Component({
-	standalone: true,
-	template: `
-		<ngt-mesh #mesh>
-			<ngt-box-geometry />
-		</ngt-mesh>
-	`,
-	schemas: [CUSTOM_ELEMENTS_SCHEMA],
-	changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class Scene {
-	mesh = viewChild.required<ElementRef<Mesh>>('mesh');
-
-	constructor() {
-		injectBeforeRender(() => {
-			const mesh = this.mesh().nativeElement;
-			mesh.rotation.x += 0.01;
-			mesh.rotation.y += 0.01;
-		});
-	}
-}
 
 @Component({
 	selector: 'platform-home',
@@ -35,7 +13,7 @@ export class Scene {
 	template: `
 		<ion-content [fullscreen]="true">
 			<div id="container">
-				<ngt-canvas [sceneGraph]="scene" />
+				<ngt-canvas [sceneGraph]="scene" [camera]="{ position: [0, -12, 16] }" [shadows]="true" />
 			</div>
 		</ion-content>
 	`,
@@ -52,5 +30,5 @@ export class Scene {
 	`,
 })
 export class HomePage {
-	scene = Scene;
+	scene = Experience;
 }
