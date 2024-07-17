4a6fd21d5dcf904bbaf75bfe677e7660c026cc7b
nartc <nartc7789@gmail.com>
2024-07-17 13:51:21 -0500

    remove examples app

--- apps/examples/src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { provideFileRouter } from '@analogjs/router';
-import type { ApplicationConfig } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
-import { withComponentInputBinding } from '@angular/router';
-
-export const appConfig: ApplicationConfig = {
-	providers: [provideFileRouter(withComponentInputBinding()), provideClientHydration()],
-};
--- apps/examples/src/app/pages/index.page.ts
+++
@@ -1,6 +0,0 @@
-import type { RouteMeta } from '@analogjs/router';
-
-export const routeMeta: RouteMeta = {
-	redirectTo: '/cubes',
-	pathMatch: 'full',
-};
