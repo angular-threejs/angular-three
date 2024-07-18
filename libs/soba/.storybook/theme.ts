552b02bd08736ada34b1d29c2a614d4b4dd3bf0e
nartc <nartc7789@gmail.com>
2024-07-18 11:30:53 -0500

    logo (thanks robin)

--- libs/soba/.storybook/theme.ts
+++ libs/soba/.storybook/theme.ts
@@ -4,6 +4,6 @@ export default create({
 	base: 'light',
 	brandTitle: 'Angular Three - Soba',
 	brandUrl: 'https://github.com/angular-threejs/angular-three',
-	brandImage: 'https://angular-threejs.netlify.com/img/ngt-logo.svg',
+	brandImage: './angular-three.svg',
 	brandTarget: '_self',
 });
