39760967f107988de12eaa87f4c21a3d5903e67b
nartc <nartc7789@gmail.com>
2024-07-19 09:46:38 -0500

    remove analogjs docs site (will make landing page analogjs instead)

--- apps/docs/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import 'zone.js';
-
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
-
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
