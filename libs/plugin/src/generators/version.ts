18b9a9e79ce5737415ab3c781b895af9fa617d0d
nartc <nartc7789@gmail.com>
2024-09-01 20:56:34 -0500

    fix(plugin): bump threejs version

--- libs/plugin/src/generators/version.ts
+++ libs/plugin/src/generators/version.ts
@@ -1,6 +1,6 @@
 export const ANGULAR_THREE_VERSION = '^2.0.0';
-export const THREE_VERSION = '^0.166.0';
-export const THREE_TYPE_VERSION = '^0.166.0';
+export const THREE_VERSION = '^0.168.0';
+export const THREE_TYPE_VERSION = '^0.168.0';
 export const NGXTENSION_VERSION = '^4.0.0';
 
 export const SOBA_PEER_DEPENDENCIES = {
