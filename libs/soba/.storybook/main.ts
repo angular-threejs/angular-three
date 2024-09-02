4017d26f94c02bcad40a00d7331db0540287c7c9
Brandon Roberts <robertsbt@gmail.com>
2024-09-01 23:21:46 -0500

    build: migrate soba Storybook to Vite

--- libs/soba/.storybook/main.ts
+++ libs/soba/.storybook/main.ts
@@ -1,33 +1,46 @@
-import type { StorybookConfig } from '@storybook/angular';
-import { resolve } from 'node:path';
+import { StorybookConfig } from '@storybook/angular';
+import { StorybookConfigVite } from '@storybook/builder-vite';
+import { UserConfig } from 'vite';
 
-const config: StorybookConfig = {
-    stories: ['../**/*.mdx', '../**/*.stories.@(js|ts)'],
-    addons: [
-        '@storybook/addon-essentials',
-        'storybook-addon-deep-controls',
-        '@chromatic-com/storybook'
-    ],
+const config: StorybookConfig & StorybookConfigVite = {
+	stories: ['../**/*.mdx', '../**/*.stories.@(js|ts)'],
+	addons: ['@storybook/addon-essentials', 'storybook-addon-deep-controls', '@chromatic-com/storybook'],
+	core: {
+		builder: {
+			name: '@storybook/builder-vite',
+			options: {
+				viteConfigPath: undefined,
+			},
+		},
+	},
+	async viteFinal(config: UserConfig) {
+		// Merge custom configuration into the default config
+		const { mergeConfig } = await import('vite');
+		const { default: angular } = await import('@analogjs/vite-plugin-angular');
+		const { nxViteTsPaths } = await import('@nx/vite/plugins/nx-tsconfig-paths.plugin');
 
-    webpackFinal: async (config) => {
-		config.module?.rules?.push({
-			test: /\.(glsl|vs|fs|vert|frag)$/,
-			exclude: /node_modules/,
-			use: ['raw-loader', 'glslify-loader'],
-			include: resolve(__dirname, '../'),
+		return mergeConfig(config, {
+			// Add dependencies to pre-optimization
+			optimizeDeps: {
+				include: [
+					'@storybook/angular',
+					'@storybook/angular/dist/client',
+					'@angular/compiler',
+					'@mdx-js/react',
+					'@storybook/blocks',
+					'tslib',
+				],
+			},
+			plugins: [angular({ jit: true, tsconfig: './.storybook/tsconfig.json' }), nxViteTsPaths()],
 		});
-
-		return config;
 	},
 
-    staticDirs: ['./public', './public/cube'],
-
-    framework: {
+	framework: {
 		name: '@storybook/angular',
 		options: {},
 	},
 
-    docs: {}
+	docs: {},
 };
 
 export default config;
