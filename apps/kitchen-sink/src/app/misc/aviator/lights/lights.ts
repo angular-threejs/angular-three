0beec444b23439a71fb2968b4cbbf2d22fddead6
nartc <nartc7789@gmail.com>
2024-09-23 00:06:14 -0500

    docs: adjust aviator demo

--- apps/kitchen-sink/src/app/misc/aviator/lights/lights.ts
+++ apps/kitchen-sink/src/app/misc/aviator/lights/lights.ts
@@ -5,14 +5,9 @@ import { NgtArgs } from 'angular-three';
 	selector: 'app-lights',
 	standalone: true,
 	template: `
-		<ngt-hemisphere-light [color]="'#aaaaaa'" [groundColor]="'#000000'" [intensity]="Math.PI * 0.9" />
-		<ngt-ambient-light [color]="'#dc8874'" [intensity]="Math.PI * 0.5" />
-		<ngt-directional-light
-			[color]="'#ffffff'"
-			[position]="[150, 350, 350]"
-			[castShadow]="true"
-			[intensity]="Math.PI * 0.9"
-		>
+		<ngt-hemisphere-light color="#aaaaaa" groundColor="#000000" [intensity]="Math.PI * 0.9" />
+		<ngt-ambient-light color="#dc8874" [intensity]="Math.PI * 0.5" />
+		<ngt-directional-light color="#ffffff" [position]="[150, 350, 350]" [castShadow]="true" [intensity]="Math.PI * 0.9">
 			<ngt-orthographic-camera *args="[-400, 400, 400, -400, 1, 1000]" attach="shadow.camera" />
 			<ngt-vector2 *args="[4096, 4096]" attach="shadow.mapSize" />
 		</ngt-directional-light>
