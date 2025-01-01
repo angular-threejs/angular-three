55a003771aac3e5d3ff34847d0b847fde4517dd9
nartc <nartc7789@gmail.com>
2024-12-31 20:20:53 -0600

    docs(core): add docs for injectBeforeRender

--- libs/core/src/lib/utils/before-render.ts
+++ libs/core/src/lib/utils/before-render.ts
@@ -3,6 +3,35 @@ import { assertInjector } from 'ngxtension/assert-injector';
 import { injectStore } from '../store';
 import { NgtBeforeRenderRecord } from '../types';
 
+/**
+ * `injectBeforeRender` invokes its callback on every frame. Hence, the notion of tracking
+ * changes (i.e: signals) does not really matter since we're getting latest values of the things we need on every frame anyway.
+ *
+ * If `priority` is dynamic, consumers should set up `injectBeforeRender` in
+ * an `effect` and track `priority` changes. Make use of `onCleanup` to clean up
+ * previous before render subscription
+ *
+ * @example
+ * ```ts
+ * const injector = inject(Injector);
+ *
+ * effect((onCleanup) => {
+ *   const priority = this.priority(); // track priority
+ *
+ *   const sub = injectBeforeRender(
+ *    ({ gl, camera }) => {
+ *      // before render logic
+ *    },
+ *    {
+ *      priority,
+ *      injector, // injector is needed since injectBeforeRender is invoked in effect body
+ *    }
+ *   });
+ *
+ *   onCleanup(() => sub());
+ * });
+ * ```
+ */
 export function injectBeforeRender(
 	cb: NgtBeforeRenderRecord['callback'],
 	{ priority = 0, injector }: { priority?: number; injector?: Injector } = {},
