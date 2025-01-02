9e7ae5a1ff425655e6bb21b17094a67903df4653
nartc <nartc7789@gmail.com>
2025-01-02 17:07:02 -0600

    chore(soba): disable doc template for now

--- libs/soba/.storybook/preview.ts
+++ libs/soba/.storybook/preview.ts
@@ -1,12 +1,10 @@
 import { Preview } from '@storybook/angular';
-// @ts-expect-error - we can import mdx
-import DocumentationTemplate from './documentation-template.mdx';
 
 export default {
 	parameters: {
 		deepControls: { enabled: true },
 		docs: {
-			page: DocumentationTemplate,
+			// page: DocumentationTemplate,
 		},
 	},
 	tags: ['autodocs'],
