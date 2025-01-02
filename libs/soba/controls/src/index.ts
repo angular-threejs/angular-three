0db0b1e99d350f520fbb19c891ef283906aaa5ca
nartc <nartc7789@gmail.com>
2025-01-02 17:07:28 -0600

    feat(soba): remove ngtspivotcontrols export from controls entry point
    BREAKING CHANGE: use `NgtsPivotControls` from `gizmos` entry point

--- libs/soba/controls/src/index.ts
+++ libs/soba/controls/src/index.ts
@@ -1,17 +1,3 @@
 export * from './lib/camera-controls';
 export * from './lib/orbit-controls';
 export * from './lib/scroll-controls';
-
-import {
-	NgtsPivotControls as GizmosPivotControls,
-	NgtsPivotControlsOptions as GizmosPivotControlsOptions,
-} from 'angular-three-soba/gizmos';
-
-/**
- * @deprecated Use `NgtsPivotControls` from `angular-three-soba/gizmos` instead.
- */
-export const NgtsPivotControls = GizmosPivotControls;
-/**
- * @deprecated Use `NgtsPivotControlsOptions` from `angular-three-soba/gizmos` instead.
- */
-export type NgtsPivotControlsOptions = GizmosPivotControlsOptions;
