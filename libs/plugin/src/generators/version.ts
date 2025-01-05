746039bdaff3f30a3d038ee01f4d5a7142564b0f
nartc <nartc7789@gmail.com>
2025-01-05 00:46:05 -0600

    fix(core): adjust versions for generator

--- libs/plugin/src/generators/version.ts
+++ libs/plugin/src/generators/version.ts
@@ -1,6 +1,6 @@
-export const ANGULAR_THREE_VERSION = '^2.0.0';
-export const THREE_VERSION = '^0.170.0';
-export const THREE_TYPE_VERSION = '^0.170.0';
+export const ANGULAR_THREE_VERSION = '^3.0.0';
+export const THREE_VERSION = '^0.171.0';
+export const THREE_TYPE_VERSION = '^0.171.0';
 export const NGXTENSION_VERSION = '^4.0.0';
 
 export const SOBA_PEER_DEPENDENCIES = {
@@ -9,10 +9,10 @@ export const SOBA_PEER_DEPENDENCIES = {
 	'@monogrid/gainmap-js': '^3.0.0',
 	'camera-controls': '^2.8.0',
 	'hls.js': '^1.5.0',
-	maath: '^0.10.0',
+	maath: '~0.10.0',
 	meshline: '^3.1.0',
 	'stats-gl': '^2.0.0',
-	'three-custom-shader-material': '^6.2.0',
-	'three-mesh-bvh': '^0.8.0',
-	'troika-three-text': '^0.52.0',
+	'three-custom-shader-material': '~6.2.0',
+	'three-mesh-bvh': '~0.8.0',
+	'troika-three-text': '~0.52.0',
 } as const;
