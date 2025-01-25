2254d42840e98a14fe1d4f2ec45589918ff511b0
nartc <nartc7789@gmail.com>
2025-01-25 17:22:16 -0600

    chore: remove ionic app

--- apps/ionic-app/src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
-import { appRoutes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(appRoutes), importProvidersFrom(IonicModule.forRoot())],
-};
