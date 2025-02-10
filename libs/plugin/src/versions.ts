f33aa1b21427c386043fb46dcb0007130814f4cc
nartc <nartc7789@gmail.com>
2025-02-10 15:43:13 -0600

    fix(plugin): use next version for ngt

--- libs/plugin/src/versions.ts
+++ libs/plugin/src/versions.ts
@@ -1,4 +1,4 @@
-export const ANGULAR_THREE_VERSION = '^4.0.0';
+export const ANGULAR_THREE_VERSION = 'next';
 export const THREE_VERSION = '^0.173.0';
 export const THREE_TYPE_VERSION = '^0.173.0';
 export const NGXTENSION_VERSION = '^4.0.0';
