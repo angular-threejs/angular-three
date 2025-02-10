32a1a98e8355b233b365de6cbdc59520422f9219
nartc <nartc7789@gmail.com>
2025-02-10 09:56:30 -0600

    chore: prep new plugin for angular-three-plugin
    new plugin

--- libs/plugin-old/jest.config.ts
+++ libs/plugin-old/jest.config.ts
@@ -1,10 +1,10 @@
+/* eslint-disable */
 export default {
 	displayName: 'plugin',
 	preset: '../../jest.preset.js',
-	testEnvironment: 'node',
 	transform: {
 		'^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
 	},
 	moduleFileExtensions: ['ts', 'js', 'html'],
-	coverageDirectory: '../../coverage/libs/plugin',
+	coverageDirectory: '../../coverage/libs/plugin-old',
 };
