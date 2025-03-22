48ba0c7a73366b695fb227cac31c800a5983fea4
nartc <nartc7789@gmail.com>
2025-03-22 09:13:35 -0500

    refactor(soba/loaders): use fontResource in injectFont

--- libs/soba/loaders/src/lib/font-loader.ts
+++ libs/soba/loaders/src/lib/font-loader.ts
@@ -1,42 +1,6 @@
-import { effect, Injector, signal } from '@angular/core';
+import { Injector } from '@angular/core';
 import { assertInjector } from 'ngxtension/assert-injector';
-import { Font, FontLoader } from 'three-stdlib';
-
-export type Glyph = {
-	_cachedOutline: string[];
-	ha: number;
-	o: string;
-};
-
-export type FontData = {
-	boundingBox: {
-		yMax: number;
-		yMin: number;
-	};
-	familyName: string;
-	glyphs: {
-		[k: string]: Glyph;
-	};
-	resolution: number;
-	underlineThickness: number;
-};
-
-export type NgtsFontInput = string | FontData;
-
-let fontLoader: FontLoader | null = null;
-
-export async function loadFontData(font: NgtsFontInput): Promise<FontData> {
-	return typeof font === 'string' ? await (await fetch(font)).json() : font;
-}
-
-export function parseFontData(fontData: FontData) {
-	if (!fontLoader) {
-		fontLoader = new FontLoader();
-	}
-	return fontLoader.parse(fontData);
-}
-
-const cache = new Map<NgtsFontInput, Font>();
+import { fontResource, type NgtsFontInput } from './font-resource';
 
 /**
  * @deprecated Use fontResource instead. Will be removed in v5.0.0
@@ -44,37 +8,14 @@ const cache = new Map<NgtsFontInput, Font>();
  */
 export function injectFont(input: () => NgtsFontInput, { injector }: { injector?: Injector } = {}) {
 	return assertInjector(injectFont, injector, () => {
-		const font = signal<Font | null>(null);
-
-		effect(() => {
-			const fontInput = input();
-
-			if (cache.has(fontInput)) {
-				font.set(cache.get(fontInput) as Font);
-				return;
-			}
-
-			loadFontData(input()).then((data) => {
-				const parsed = parseFontData(data);
-				cache.set(fontInput, parsed);
-				font.set(parsed);
-			});
-		});
-
-		return font.asReadonly();
+		const resource = fontResource(input, { injector });
+		return resource.value.asReadonly();
 	});
 }
 
 injectFont.preload = (input: () => NgtsFontInput) => {
-	loadFontData(input()).then((data) => {
-		const parsed = parseFontData(data);
-		cache.set(input(), parsed);
-	});
+	fontResource.preload(input());
 };
 injectFont.clear = (input?: () => NgtsFontInput) => {
-	if (input) {
-		cache.delete(input());
-	} else {
-		cache.clear();
-	}
+	fontResource.clear(input?.());
 };
--- libs/soba/loaders/src/lib/font-resource.ts
+++ libs/soba/loaders/src/lib/font-resource.ts
@@ -1,7 +1,40 @@
 import { Injector, resource } from '@angular/core';
 import { assertInjector } from 'ngxtension/assert-injector';
-import { Font } from 'three-stdlib';
-import { loadFontData, NgtsFontInput, parseFontData } from './font-loader';
+import { Font, FontLoader } from 'three-stdlib';
+
+type Glyph = {
+	_cachedOutline: string[];
+	ha: number;
+	o: string;
+};
+
+type FontData = {
+	boundingBox: {
+		yMax: number;
+		yMin: number;
+	};
+	familyName: string;
+	glyphs: {
+		[k: string]: Glyph;
+	};
+	resolution: number;
+	underlineThickness: number;
+};
+
+export type NgtsFontInput = string | FontData;
+
+let fontLoader: FontLoader | null = null;
+
+async function loadFontData(font: NgtsFontInput): Promise<FontData> {
+	return typeof font === 'string' ? await (await fetch(font)).json() : font;
+}
+
+function parseFontData(fontData: FontData) {
+	if (!fontLoader) {
+		fontLoader = new FontLoader();
+	}
+	return fontLoader.parse(fontData);
+}
 
 const cache = new Map<NgtsFontInput, Font>();
 
