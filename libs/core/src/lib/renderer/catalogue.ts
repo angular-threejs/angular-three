651cfe2bb00daccfe55754b467a4c5b92d33413f
nartc <nartc7789@gmail.com>
2025-03-24 20:49:20 -0500

    feat(core): add fn to remove keys from catalogue; extend returns a clean up fn

--- libs/core/src/lib/renderer/catalogue.ts
+++ libs/core/src/lib/renderer/catalogue.ts
@@ -3,8 +3,18 @@ import type { NgtConstructorRepresentation } from '../types';
 
 const catalogue: Record<string, NgtConstructorRepresentation> = {};
 
-export function extend(objects: object): void {
+export function extend(objects: object) {
+	const keys = Object.keys(objects);
 	Object.assign(catalogue, objects);
+	return () => {
+		remove(...keys);
+	};
+}
+
+export function remove(...keys: string[]) {
+	for (const key of keys) {
+		delete catalogue[key];
+	}
 }
 
 export const NGT_CATALOGUE = new InjectionToken<typeof catalogue>('NGT_CATALOGUE', { factory: () => catalogue });
