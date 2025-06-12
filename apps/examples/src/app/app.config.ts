9e190eb832cd162e32db1cc9a3640677a2bedac6
Chau <25516557+nartc@users.noreply.github.com>
2025-06-12 17:16:14 -0500

    docs(examples): ng20 zoneless provider

--- apps/examples/src/app/app.config.ts
+++ apps/examples/src/app/app.config.ts
@@ -1,11 +1,11 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideNgtRenderer } from 'angular-three/dom';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideExperimentalZonelessChangeDetection(),
+		provideZonelessChangeDetection(),
 		provideRouter(appRoutes, withComponentInputBinding()),
 		provideNgtRenderer(),
 	],
