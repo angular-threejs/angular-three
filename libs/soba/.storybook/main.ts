ea336b0b57f101ec1a33a92ac8788191ca9e4f73
Chau <25516557+nartc@users.noreply.github.com>
2025-12-11 14:33:57 +0700

    chore(repo): bump nx

--- libs/soba/.storybook/main.ts
+++ libs/soba/.storybook/main.ts
@@ -1,3 +1,4 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
 import { StorybookConfig } from '@storybook/angular';
 import { createRequire } from 'node:module';
 import { dirname, join } from 'node:path';
